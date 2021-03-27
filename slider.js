80c4495e80f1b0618a6ed90f04ab8267ce002fa0
m4mohammadkhalid <61066087+m4mohammadkhalid@users.noreply.github.com>
2021-03-28 04:16:19 +0530

    updates

--- slider.js
+++ slider.js
@@ -1,103 +1,89 @@
-class CitiesSlider extends React.Component {
-  constructor(props) {
-    super(props);
+// Params
+let mainSliderSelector = '.main-slider',
+    navSliderSelector = '.nav-slider',
+    interleaveOffset = 0.5;
 
-    this.IMAGE_PARTS = 4;
+// Main Slider
+let mainSliderOptions = {
+      loop: true,
+      speed:1000,
+      autoplay:{
+        delay:3000
+      },
+      loopAdditionalSlides: 10,
+      grabCursor: true,
+      watchSlidesProgress: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        init: function(){
+          this.autoplay.stop();
+        },
+        imagesReady: function(){
+          this.el.classList.remove('loading');
+          this.autoplay.start();
+        },
+        slideChangeTransitionEnd: function(){
+          let swiper = this,
+              captions = swiper.el.querySelectorAll('.caption');
+          for (let i = 0; i < captions.length; ++i) {
+            captions[i].classList.remove('show');
+          }
+          swiper.slides[swiper.activeIndex].querySelector('.caption').classList.add('show');
+        },
+        progress: function(){
+          let swiper = this;
+          for (let i = 0; i < swiper.slides.length; i++) {
+            let slideProgress = swiper.slides[i].progress,
+                innerOffset = swiper.width * interleaveOffset,
+                innerTranslate = slideProgress * innerOffset;
+           
+            swiper.slides[i].querySelector(".slide-bgimg").style.transform =
+              "translateX(" + innerTranslate + "px)";
+          }
+        },
+        touchStart: function() {
+          let swiper = this;
+          for (let i = 0; i < swiper.slides.length; i++) {
+            swiper.slides[i].style.transition = "";
+          }
+        },
+        setTransition: function(speed) {
+          let swiper = this;
+          for (let i = 0; i < swiper.slides.length; i++) {
+            swiper.slides[i].style.transition = speed + "ms";
+            swiper.slides[i].querySelector(".slide-bgimg").style.transition =
+              speed + "ms";
+          }
+        }
+      }
+    };
+let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
 
-    this.changeTO = null;
-    this.AUTOCHANGE_TIME = 4000;
+// Navigation Slider
+let navSliderOptions = {
+      loop: true,
+      loopAdditionalSlides: 10,
+      speed:1000,
+      spaceBetween: 5,
+      slidesPerView: 5,
+      centeredSlides : true,
+      touchRatio: 0.2,
+      slideToClickedSlide: true,
+      direction: 'vertical',
+      on: {
+        imagesReady: function(){
+          this.el.classList.remove('loading');
+        },
+        click: function(){
+          mainSlider.autoplay.stop();
+        }
+      }
+    };
+let navSlider = new Swiper(navSliderSelector, navSliderOptions);
 
-    this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
-  }
-
-  componentWillUnmount() {
-    window.clearTimeout(this.changeTO);
-  }
-
-  componentDidMount() {
-    this.runAutochangeTO();
-    setTimeout(() => {
-      this.setState({ activeSlide: 0, sliderReady: true });
-    }, 0);
-  }
-
-  runAutochangeTO() {
-    this.changeTO = setTimeout(() => {
-      this.changeSlides(1);
-      this.runAutochangeTO();
-    }, this.AUTOCHANGE_TIME);
-  }
-
-  changeSlides(change) {
-    window.clearTimeout(this.changeTO);
-    const { length } = this.props.slides;
-    const prevSlide = this.state.activeSlide;
-    let activeSlide = prevSlide + change;
-    if (activeSlide < 0) activeSlide = length - 1;
-    if (activeSlide >= length) activeSlide = 0;
-    this.setState({ activeSlide, prevSlide });
-  }
-
-  render() {
-    const { activeSlide, prevSlide, sliderReady } = this.state;
-    return /*#__PURE__*/(
-      React.createElement("div", { className: classNames('slider', { 's--ready': sliderReady }) }, /*#__PURE__*/
-      React.createElement("p", { className: "slider__top-heading" }, "Travelers"), /*#__PURE__*/
-      React.createElement("div", { className: "slider__slides" },
-      this.props.slides.map((slide, index) => /*#__PURE__*/
-      React.createElement("div", {
-        className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
-        key: slide.city }, /*#__PURE__*/
-
-      React.createElement("div", { className: "slider__slide-content" }, /*#__PURE__*/
-      React.createElement("h3", { className: "slider__slide-subheading" }, slide.country || slide.city), /*#__PURE__*/
-      React.createElement("h2", { className: "slider__slide-heading" },
-      slide.city.split('').map(l => /*#__PURE__*/React.createElement("span", null, l))), /*#__PURE__*/
-
-      React.createElement("p", { className: "slider__slide-readmore" }, "read more")), /*#__PURE__*/
-
-      React.createElement("div", { className: "slider__slide-parts" },
-      [...Array(this.IMAGE_PARTS).fill()].map((x, i) => /*#__PURE__*/
-      React.createElement("div", { className: "slider__slide-part", key: i }, /*#__PURE__*/
-      React.createElement("div", { className: "slider__slide-part-inner", style: { backgroundImage: `url(${slide.img})` } }))))))), /*#__PURE__*/
-
-
-
-
-
-
-      React.createElement("div", { className: "slider__control", onClick: () => this.changeSlides(-1) }), /*#__PURE__*/
-      React.createElement("div", { className: "slider__control slider__control--right", onClick: () => this.changeSlides(1) })));
-
-
-  }}
-
-
-const slides = [
-{
-  city: 'Paris',
-  country: 'France',
-  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg' },
-
-{
-  city: 'Singapore',
-  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg' },
-
-{
-  city: 'Prague',
-  country: 'Czech Republic',
-  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg' },
-
-{
-  city: 'Amsterdam',
-  country: 'Netherlands',
-  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg' },
-
-{
-  city: 'Moscow',
-  country: 'Russia',
-  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg' }];
-
-
-
-ReactDOM.render( /*#__PURE__*/React.createElement(CitiesSlider, { slides: slides }), document.querySelector('#app'));
+// Matching sliders
+mainSlider.controller.control = navSlider;
+navSlider.controller.control = mainSlider;
